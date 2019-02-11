6c19ddc5d976fd1182c6226442e8b2a33069bd1a
ribo <roibeard.mac.unfraidh@gmail.com>
2019-02-11 12:16:01 +0100

    ribo0.01: Implement bootstrap slideshow and button.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-alert("anseo");
+// alert("anseo");
+
+
+$(document).ready(function () {
+
+  // alert("hey");
+
+  $(".about").click(function () {
+    console.log("hey");
+    $(".amergin").css("display", "block");
+
+  });
+
+
+  /* about carousel */
+
+  $('.carousel').carousel({
+    pause: false,
+    interval: 4000
+  })
+
+
+});
