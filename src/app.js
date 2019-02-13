d5802fa1e6fb219b838edf792e5d801216d8d9a6
ribo <roibeard.mac.unfraidh@gmail.com>
2019-02-13 14:00:28 +0100

    ribo0.01: Reorganize html elements.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 // alert("anseo");
 
-
+// let slideshowlength = $(".item").length;
 $(document).ready(function () {
+  let current = $('.active').index();
+
 
   // alert("hey");
+  aboutGo = () => {
+    $(".amergin").fadeTo("slow", 1)
 
+  }
   $(".about").click(function () {
-    console.log("hey");
-    $(".amergin").css("display", "block");
+    console.log("hey" + current);
+
+    $('.carousel').carousel(0);
+    $("#splash").fadeOut();
+
+    setTimeout(aboutGo(), 4000);
 
   });
 
@@ -15,9 +24,19 @@ $(document).ready(function () {
   /* about carousel */
 
   $('.carousel').carousel({
-    pause: false,
-    interval: 4000
+    pause: true,
+    interval: 4000,
   })
+  // runSpecificSlideBehaviors = (current) => {
 
+  //   if ($('.carousel').carousel(0 || 1 || 2)) {
+  //     console.log("0 1 2...")
+  //   }
+  //   else
+  //     if ($('.carousel').carousel(3 || 4 || 5)) {
+  //       console.log("3 4 5...")
+  //     }
+  // }
+  // runSpecificSlideBehaviors(current)
 
 });
