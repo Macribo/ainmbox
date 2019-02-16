0832a472dd39534dc1e16ad2f674bf3355ed2aea
ribo <roibeard.mac.unfraidh@gmail.com>
2019-02-16 13:54:55 +0100

    ribo0.01: Override slide change interval; enable active slide actions with switch statement.

--- src/app.js
+++ src/app.js
@@ -1,42 +1,104 @@
-// alert("anseo");
 
-// let slideshowlength = $(".item").length;
 $(document).ready(function () {
-  let current = $('.active').index();
+  slideEvent = () => {
+    switch (slideNumber) {
 
+      case 0: console.log("First poem is...");
+        $('.carousel').carousel(0);
+        wait = 6000;
+        break;
 
-  // alert("hey");
-  aboutGo = () => {
-    $(".amergin").fadeTo("slow", 1)
+      case 1: console.log("sampla");
+        wait = 4000;
+        $('.carousel').carousel(1);
+
+        break;
+      case 2: console.log("according to legends");
+        $('.carousel').carousel(2);
+        break;
+
+      case 3: console.log("nexto");
+        $('.carousel').carousel(3);
+
+        break;
+      case 4: console.log("and then...");
+        wait = 1000;
+        $('.carousel').carousel(4);
+
+        break;
+
+      case 5: console.log("and then...");
+        wait = 4000;
+        $('.carousel').carousel(5);
+
+        break;
+      case 6: console.log("and then...");
+        $('.carousel').carousel(6);
+
+        break;
+      case 7: console.log("and then...");
+        $('.carousel').carousel(7);
+
+        break;
+      case 8: console.log("and then...");
+        $('.carousel').carousel(8);
+
+        break;
+      case 9: console.log("and then...");
+        $('.carousel').carousel(9);
+
+        break;
+      case 10: console.log("and then...");
+        $('.carousel').carousel(10);
+
+        break;
+
+      default: break;
+
+    }
 
   }
-  $(".about").click(function () {
-    console.log("hey" + current);
 
+  var darkpic = './images/backgrounds/bg_Dark.png';
+
+
+  $('.carousel').carousel({
+    pause: false,
+    interval: 0,
+
+  })
+
+  let playa = false;
+
+  if (playa) {
+    $('.carousel').carousel('cycle');
+
+  } else {
+    $('.carousel').carousel('pause');
+
+  }
+
+  aboutGo = () => {
+    playa = true;
     $('.carousel').carousel(0);
-    $("#splash").fadeOut();
+    $("#holdSlide").fadeTo("slow", 1);
+    document.getElementById('bg-img1').src = darkpic;
+  }
 
-    setTimeout(aboutGo(), 4000);
+  $(".about").click(function () {
+
+    aboutGo();
 
   });
 
+  var slideNumber = 0;
+  var wait = 4000;
+  changeSlide = () => {
 
-  /* about carousel */
-
-  $('.carousel').carousel({
-    pause: true,
-    interval: 4000,
-  })
-  // runSpecificSlideBehaviors = (current) => {
-
-  //   if ($('.carousel').carousel(0 || 1 || 2)) {
-  //     console.log("0 1 2...")
-  //   }
-  //   else
-  //     if ($('.carousel').carousel(3 || 4 || 5)) {
-  //       console.log("3 4 5...")
-  //     }
-  // }
-  // runSpecificSlideBehaviors(current)
-
+    setTimeout(changeSlide, wait);
+    slideEvent();
+    slideNumber++;
+    ;
+  }
+  changeSlide();
 });
