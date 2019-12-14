413883145432ed762125273cdac27241dc58ded1
macribo <roibeard.mac.unfraidh@gmail.com>
2019-12-14 17:15:28 +0100

    add link to ritt_web

--- src/app.js
+++ src/app.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
       // alert();
       window.location.href = "https://macribo.github.io/tall-order/public/index.html";
   });
+
+  $('#btn-gaeilge').on('click', function(){
+    // alert();
+    window.location.href = "https://macribo.github.io/ritt_web/index.html";
+});
 });
