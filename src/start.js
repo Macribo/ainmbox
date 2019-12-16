82195f671255574450d719dec5c2ae9c84c1869f
macribo <roibeard.mac.unfraidh@gmail.com>
2019-12-16 14:12:12 +0100

    remove boostrap btn styling.

--- src/start.js
+++ src/start.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   temp = document.getElementById("temp");
 
-  temp.onclick = function () {
-    console.log("temp greetings")
-    req = temp.requestFullScreen || temp.webkitRequestFullScreen || temp.mozRequestFullScreen;
-    req.call(temp);
-    $('#btn-menu').fadeOut();
-  }
-
+  // temp.onclick = function () {
+  //   console.log("temp greetings")
+  //   req = temp.requestFullScreen || temp.webkitRequestFullScreen || temp.mozRequestFullScreen;
+  //   req.call(temp);
+  //   $('#btn-menu').fadeOut();
+  // }
+// 
 
 });
