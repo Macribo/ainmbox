631018732562d7446ea05cee824955959fe8937d
macribo <roibeard.mac.unfraidh@gmail.com>
2019-12-29 14:30:59 +0100

    Link Eacht to char_select.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   
     $('#btn-tall-order').on('click', function(){
       // alert();
-      window.location.href = "https://macribo.github.io/tall-order/public/index.html";
+      window.location.href = "https://macribo.github.io/char_select";
   });
 
   $('#btn-gaeilge').on('click', function(){
